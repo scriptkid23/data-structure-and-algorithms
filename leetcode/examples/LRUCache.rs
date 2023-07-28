81bc5b07f89b581acc016b0ea84a7bb2e93709d4
hoando <hoando.dev@gmail.com>
2023-07-28 16:07:40 +0700

    fix: failed to get key

--- leetcode/examples/LRUCache.rs
+++ leetcode/examples/LRUCache.rs
@@ -23,7 +23,7 @@ impl Node {
 
 impl PartialEq for Node {
     fn eq(&self, other: &Self) -> bool {
-        self.clone().value == other.clone().value && self.clone().key == other.clone().key
+        self.clone().key == other.clone().key
     }
 }
 
@@ -63,6 +63,7 @@ impl Queue {
 
         return node;
     }
+
     fn remove(&mut self, node: Link) {
         let node_prev = node.as_ref().unwrap().borrow().prev.clone();
         let node_next = node.as_ref().unwrap().borrow().next.clone();
@@ -89,10 +90,10 @@ impl Queue {
             return None;
         }
 
-        let mut value: Option<i32> = None;
+        let mut key: Option<i32> = None;
         match self.front.clone().take() {
             Some(x) => {
-                value = Some(x.borrow().clone().value);
+                key = Some(x.borrow().clone().key);
             }
             None => {}
         }
@@ -115,7 +116,7 @@ impl Queue {
             }
         }
 
-        return value;
+        return key;
     }
 }
 
@@ -135,6 +136,7 @@ struct LRUCache {
     capacity: i32,
     ht: HashMap<i32, Link>,
     q: Queue,
+    size: i32,
 }
 
 impl Debug for Queue {
@@ -155,26 +157,23 @@ impl LRUCache {
             capacity: capacity,
             ht: HashMap::new(),
             q: Queue::new(),
+            size:0,
         };
     }
 
     fn get(&mut self, key: i32) -> i32 {
         let value = self.ht.get(&key);
+        
+        println!("Value: {:?}",value);
         let mut result: i32 = -1;
-        match value.clone().take() {
-            Some(x) => {
-                let mut buffer = x.clone();
-                match buffer.take() {
-                    Some(b) => {
-                        result = b.borrow().clone().value;
-                        self.q.remove(Some(b));
-                        self.q.push_front(key, result);
-                    }
-                    None => {}
-                }
+        if let Some(x) = value.clone().take() {
+            if let Some(b) = x.clone().take() {
+                result = b.borrow().clone().value;
+                self.q.remove(Some(b));
+                self.q.push_front(key, result);
             }
-            None => {}
         }
+        
         return result;
     }
 
@@ -182,14 +181,20 @@ impl LRUCache {
         let entry = self.ht.get(&key);
 
         if entry.is_none() {
-            if self.ht.len() >= self.capacity as usize {
+            if self.size >= self.capacity  {
+             
                 let key_to_remove = self.q.pop_back().unwrap();
-                self.ht.remove_entry(&key_to_remove);
+                println!("trigger with {}", key_to_remove);
+                self.size -= 1;
+
+                self.ht.remove(&key_to_remove);
             }
 
             let value = self.q.push_front(key, value);
 
             self.ht.insert(key, Some(value));
+
+            self.size +=1;
         } else {
             self.q.remove(entry.unwrap().clone());
             self.q.push_front(key, value);
@@ -199,13 +204,12 @@ impl LRUCache {
 fn main() {
     let mut lRUCache = LRUCache::new(2);
     lRUCache.put(1, 0); // cache is {1=1}
-    lRUCache.put(2, 2); // cache is {1=1}
-    lRUCache.put(3, 3);
-    lRUCache.get(2);
-    lRUCache.put(4, 4);
-
-    print!("{}", lRUCache.get(1));
-    // lRUCache.get(2);
-
-    print!("{:?}", lRUCache.q);
+    lRUCache.put(2, 29999); // cache is {1=1}
+    lRUCache.put(3, 3999);
+    lRUCache.put(4, 3999);
+   
+    print!("{}",lRUCache.get(1));
+    println!("Hash talbe {:?}",lRUCache.ht);
+    println!("Queue {:?}",lRUCache. q);
+    
 }
