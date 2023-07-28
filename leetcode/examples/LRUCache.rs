f646c248d83cb147ffa2d13ae4d4da6685290842
hoando <hoando.dev@gmail.com>
2023-07-28 15:41:28 +0700

    update

--- leetcode/examples/LRUCache.rs
+++ leetcode/examples/LRUCache.rs
@@ -1,17 +1,19 @@
-use std::{cell::RefCell, collections::HashMap, rc::Rc};
+use std::{cell::RefCell, collections::HashMap, fmt::Debug, rc::Rc};
 
 type Link = Option<Rc<RefCell<Node>>>;
 
-#[derive(Clone, Debug)]
+#[derive(Clone)]
 struct Node {
+    key: i32,
     value: i32,
     next: Link,
     prev: Link,
 }
 
 impl Node {
-    fn new(value: i32) -> Rc<RefCell<Node>> {
+    fn new(key: i32, value: i32) -> Rc<RefCell<Node>> {
         Rc::new(RefCell::new(Node {
+            key,
             value,
             next: None,
             prev: None,
@@ -19,24 +21,33 @@ impl Node {
     }
 }
 
+impl PartialEq for Node {
+    fn eq(&self, other: &Self) -> bool {
+        self.clone().value == other.clone().value && self.clone().key == other.clone().key
+    }
+}
+
+impl Debug for Node {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        print!("{}", self.clone().key);
+        Ok(())
+    }
+}
 struct Queue {
     front: Link,
     back: Link,
-    ht: HashMap<i32, Link>,
 }
+
 impl Queue {
     pub fn new() -> Queue {
         Queue {
             front: None,
             back: None,
-            ht: HashMap::new(),
         }
     }
 
-    pub fn push_front(&mut self, val: i32) {
-        let node = Node::new(val);
-
-        self.ht.insert(val, Some(node.clone()));
+    pub fn push_front(&mut self, key: i32, value: i32) -> Rc<RefCell<Node>> {
+        let node = Node::new(key, value);
 
         match self.back.take() {
             Some(old) => {
@@ -48,20 +59,50 @@ impl Queue {
                 self.front = Some(node.clone());
             }
         }
-        self.back = Some(node);
+        self.back = Some(node.clone());
+
+        return node;
     }
-    fn pop_back(&mut self) {
-        if self.front.is_none() {
-            return;
+    fn remove(&mut self, node: Link) {
+        let node_prev = node.as_ref().unwrap().borrow().prev.clone();
+        let node_next = node.as_ref().unwrap().borrow().next.clone();
+
+        if let Some(node_prev) = node_prev {
+            node_prev.borrow_mut().next = node.as_ref().unwrap().borrow().next.clone();
         }
-        
+
+        if let Some(node_next) = node_next {
+            node_next.borrow_mut().prev = node.as_ref().unwrap().borrow().prev.clone();
+        }
+
+        if node == self.front {
+            self.front = self.front.clone().unwrap().borrow().next.clone();
+        }
+
+        if node == self.back {
+            self.back = self.back.clone().unwrap().borrow().prev.clone();
+        }
+    }
+
+    fn pop_back(&mut self) -> Option<i32> {
+        if self.front.is_none() {
+            return None;
+        }
+
+        let mut value: Option<i32> = None;
+        match self.front.clone().take() {
+            Some(x) => {
+                value = Some(x.borrow().clone().value);
+            }
+            None => {}
+        }
+
         match self.front.take() {
             Some(old) => {
                 self.front = old.borrow().clone().to_owned().next;
             }
             None => {}
         }
-
 
         if self.front.is_none() {
             self.back = None;
@@ -74,7 +115,7 @@ impl Queue {
             }
         }
 
-     
+        return value;
     }
 }
 
@@ -89,16 +130,82 @@ fn count_nodes(queue: &Queue) -> usize {
 
     count
 }
-fn main() {
-    let mut q = Queue::new();
-    q.push_front(3);
-    q.push_front(4);
-    q.push_front(5);
-    q.push_front(6);
 
-    q.pop_back();
-    q.pop_back();
-    q.pop_back();
-    
-    println!("Number of nodes in queue: {}", count_nodes(&q));
+struct LRUCache {
+    capacity: i32,
+    ht: HashMap<i32, Link>,
+    q: Queue,
+}
+
+impl Debug for Queue {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut current = self.front.clone();
+
+        while let Some(node) = current.clone() {
+            print!("{:?}", current);
+            current = node.borrow().next.clone();
+        }
+        Ok(())
+    }
+}
+
+impl LRUCache {
+    fn new(capacity: i32) -> Self {
+        return LRUCache {
+            capacity: capacity,
+            ht: HashMap::new(),
+            q: Queue::new(),
+        };
+    }
+
+    fn get(&mut self, key: i32) -> i32 {
+        let value = self.ht.get(&key);
+        let mut result: i32 = -1;
+        match value.clone().take() {
+            Some(x) => {
+                let mut buffer = x.clone();
+                match buffer.take() {
+                    Some(b) => {
+                        result = b.borrow().clone().value;
+                        self.q.remove(Some(b));
+                        self.q.push_front(key, result);
+                    }
+                    None => {}
+                }
+            }
+            None => {}
+        }
+        return result;
+    }
+
+    fn put(&mut self, key: i32, value: i32) {
+        let entry = self.ht.get(&key);
+
+        if entry.is_none() {
+            if self.ht.len() >= self.capacity as usize {
+                let key_to_remove = self.q.pop_back().unwrap();
+                self.ht.remove_entry(&key_to_remove);
+            }
+
+            let value = self.q.push_front(key, value);
+
+            self.ht.insert(key, Some(value));
+        } else {
+            self.q.remove(entry.unwrap().clone());
+            self.q.push_front(key, value);
+        }
+    }
+}
+fn main() {
+    let mut lRUCache = LRUCache::new(2);
+    lRUCache.put(1, 0); // cache is {1=1}
+    lRUCache.put(2, 2); // cache is {1=1}
+    lRUCache.put(3, 3);
+    lRUCache.get(2);
+    lRUCache.put(4, 4);
+
+    print!("{}", lRUCache.get(1));
+    // lRUCache.get(2);
+
+    print!("{:?}", lRUCache.q);
 }
