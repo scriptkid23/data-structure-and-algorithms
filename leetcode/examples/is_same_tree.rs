60440921306589fe410f3de3ef83de67589d6644
hoando <hoando.dev@gmail.com>
2023-07-30 10:54:48 +0700

    feat: solution is_same_tree

--- leetcode/examples/is_same_tree.rs
+++ leetcode/examples/is_same_tree.rs
@@ -27,10 +27,7 @@ impl TreeNode {
         TreeNode::insert_left(&current, val);
     }
 
-    pub fn insert_right(
-        node: &Option<Rc<RefCell<TreeNode>>>,
-        val: i32,
-    )  {
+    pub fn insert_right(node: &Option<Rc<RefCell<TreeNode>>>, val: i32) {
         let current = node.as_ref().unwrap().borrow().right.clone();
 
         if current.is_none() {
@@ -51,9 +48,40 @@ impl Solution {
         p: Option<Rc<RefCell<TreeNode>>>,
         q: Option<Rc<RefCell<TreeNode>>>,
     ) -> bool {
+        let mut vec_p = Vec::new();
+        scan(&p, &mut vec_p);
 
-        print!("{:?} {:?}",p, q);
-        false
+        let mut vec_q = Vec::new();
+        scan(&q, &mut vec_q);
+
+        let result = vec_p.cmp(&vec_q);
+
+        print!("{result:?}");
+        return result.is_eq();
+    
+    }
+}
+
+pub fn scan(tree: &Option<Rc<RefCell<TreeNode>>>, vec: &mut Vec<Option<i32>>) {
+    if tree.is_none() {
+        return;
+    };
+
+    let current = tree.as_ref().unwrap().borrow_mut();
+    vec.push(Some(current.val));
+
+    if !current.left.is_none() {
+        scan(&current.left, vec);
+    }
+    else {
+        vec.push(None);
+    }
+
+    if !current.right.is_none() {
+        scan(&current.right, vec);
+    }
+    else {
+        vec.push(None);
     }
 }
 
@@ -61,13 +89,10 @@ fn main() {
     let p = Some(Rc::new(RefCell::new(TreeNode::new(1))));
     TreeNode::insert_left(&p, 2);
 
-    TreeNode::insert_right(&p, 3);
 
     let q = Some(Rc::new(RefCell::new(TreeNode::new(1))));
-    TreeNode::insert_left(&q, 2);
 
-    TreeNode::insert_right(&q, 3);
+    TreeNode::insert_right(&q, 2);
 
     Solution::is_same_tree(p, q);
-
 }
